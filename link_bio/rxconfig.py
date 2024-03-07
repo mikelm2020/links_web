26bf92a110b5627ef8c0ca7f4312253526579145
Miguel Angel Lopez <miguel.lopezm.dev@gmail.com>
2024-03-07 14:17:06 -0600

    Added CORS

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -2,4 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://my-links-web.vercel.app/",
+    ],
 )
