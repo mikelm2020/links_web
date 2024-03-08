57542d6a0fa90e213707b312c18239138477ad51
Miguel Angel Lopez <miguel.lopezm.dev@gmail.com>
2024-03-08 00:56:35 -0600

    Other changes

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -4,6 +4,6 @@ config = rx.Config(
     app_name="link_bio",
     cors_allowed_origins=[
         "http://localhost:3000",
-        "my-links-web.vercel.app",
+        "https://my-links-web.vercel.app:3000",
     ],
 )
